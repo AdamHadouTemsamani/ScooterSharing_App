4a79193eb4aca39e94482ce5ed267902ab574dba
Adam Hadou Temsamani <ahad@itu.dk>
2023-02-07 14:17:40 +0100

    Added documentation

--- app/src/main/java/dk/itu/moapd/scootersharing/ahad/Scooter.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/ahad/Scooter.kt
@@ -1,10 +1,17 @@
 package dk.itu.moapd.scootersharing.ahad
 
+/**
+ * A data class that encapsulates the information about a scooter
+ */
 data class Scooter (var name: String,
                     var location: String) {
 
+    /**
+     * Returns a formatted string with the information of the Scooter object
+     * @return Returns String
+     */
     override fun toString(): String {
-        return "[Scooter] $name is placed at $location"
+        return "Ride started using Scooter(name=$name, location=$location)"
     }
 }
 
