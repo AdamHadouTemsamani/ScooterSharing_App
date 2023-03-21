4881e8bcee7a36c24965bd74c865ae743ac212f6
Adam Hadou Temsamani <ahad@itu.dk>
2023-03-21 15:06:01 +0100

    Finished testing
    Still doens't really work :(

---
+++ app/src/androidTest/java/dk/itu/moapd/scootersharing/ahad/StartRideActivityTest.kt
@@ -0,0 +1,52 @@
+package dk.itu.moapd.scootersharing.ahad
+
+import androidx.fragment.app.testing.launchFragment
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.lifecycle.Lifecycle
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import dk.itu.moapd.scootersharing.ahad.fragments.MainFragment
+import dk.itu.moapd.scootersharing.ahad.fragments.StartRideFragment
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class StartRideActivityTest {
+
+    @get:Rule
+    var fragmentContainer = launchFragmentInContainer<StartRideFragment>()
+
+    @Test
+    fun createMainFragment() {
+        fragmentContainer.moveToState(Lifecycle.State.RESUMED)
+    }
+
+    @Test
+    fun readNameEditText() {
+        onView(withId(R.id.edit_text_name))
+            .check(matches(ViewMatchers.withText("")))
+    }
+
+    @Test
+    fun readLocationEditText() {
+        onView(withId(R.id.edit_text_location))
+            .check(matches(ViewMatchers.withText("")))
+    }
+
+    @Test
+    fun clickAddButton() {
+        onView(withId(R.id.edit_text_name))
+            .perform(clearText(), typeText("TestScooter"))
+        onView(withId(R.id.edit_text_location))
+            .perform(clearText(), typeText("ITU"))
+        onView(withId(R.id.start_ride_button))
+            .perform(click())
+    }
+}
