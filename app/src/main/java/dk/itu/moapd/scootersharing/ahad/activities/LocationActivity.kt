d79e739f03b89d82140557d56478a6efb5f58aa5
Adam Hadou Temsamani <ahad@itu.dk>
2023-04-23 17:09:58 +0200

    Begin Exercise 9.2

---
+++ app/src/main/java/dk/itu/moapd/scootersharing/ahad/activities/LocationActivity.kt
@@ -0,0 +1,7 @@
+package dk.itu.moapd.scootersharing.ahad.activities
+
+import androidx.appcompat.app.AppCompatActivity
+
+class LocationActivity : AppCompatActivity() {
+
+}
---
+++ app/src/main/java/dk/itu/moapd/scootersharing/ahad/fragments/LocationFragment.kt
@@ -0,0 +1,7 @@
+package dk.itu.moapd.scootersharing.ahad.fragments
+
+import androidx.fragment.app.Fragment
+
+class LocationFragment : Fragment() {
+
+}
